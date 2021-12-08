13652efb5280b4ddde40ad9b9d07bb5dd31c79e8
anangfatkur <anangfatkurrr@gmail.com>
2021-12-08 11:14:27 +0700

    penggunaan fitur statefull pada stateless dengan getbuilder

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final counterC = Get.put(CounterController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () =>
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+              builder: (context) => PageLain(),
+            )),
+            icon: Icon(Icons.access_alarm),
+          ),
+        ],
+      ),
+      body: Center(
+        child: CountWidget(),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        counterC.add();
+      }),
+    );
+  }
+}
+
+class CountWidget extends StatelessWidget {
+  final counterC = Get.find<CounterController>();
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<CounterController>(
+      
+      builder: (c) => Text("Angka ${c.count}"),
+    );
+  }
+}
+
+class PageLain extends StatelessWidget {
+  const PageLain({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
+
+class CounterController extends GetxController {
+  var count = 0;
+
+  void add() {
+    count++;
+    update();
+  }
+}
