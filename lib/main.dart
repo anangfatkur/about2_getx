f1030cc7ea3e7433403eafb308d7e9f759e5f68c
anangfatkur <anangfatkurrr@gmail.com>
2021-12-08 11:52:10 +0700

    simple routing getx

--- lib/main.dart
+++ lib/main.dart
@@ -8,69 +8,85 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       home: HomePage(),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
-  final counterC = Get.put(CounterController());
+  // final counterC = Get.put(CounterController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text("Home Page"),
         actions: [
           IconButton(
-            onPressed: () =>
-                Navigator.of(context).pushReplacement(MaterialPageRoute(
-              builder: (context) => PageLain(),
-            )),
+            onPressed: () => Get.to(() => PageTulisan ()),
             icon: Icon(Icons.access_alarm),
           ),
         ],
       ),
       body: Center(
-        child: CountWidget(),
+        child: Text("Home Page"),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        counterC.add();
-      }),
     );
   }
 }
 
-class CountWidget extends StatelessWidget {
-  final counterC = Get.find<CounterController>();
-  @override
-  Widget build(BuildContext context) {
-    return GetBuilder<CounterController>(
-      initState: (_) => print("initstate"),
-      didChangeDependencies: (state) => print("didChangeDependencies"),
-      didUpdateWidget: (oldWidget, state) => print("didUpdateWidget"),
-      dispose: (state) => print("dispose"),
-      builder: (c) => Text("Angka ${c.count}"),
-    );
-  }
-}
+// class CountWidget extends StatelessWidget {
+//   final counterC = Get.find<CounterController>();
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetBuilder<CounterController>(
+//       initState: (_) => print("initstate"),
+//       didChangeDependencies: (state) => print("didChangeDependencies"),
+//       didUpdateWidget: (oldWidget, state) => print("didUpdateWidget"),
+//       dispose: (state) => print("dispose"),
+//       builder: (c) => Text("Angka ${c.count}"),
+//     );
+//   }
+// }
 
-class PageLain extends StatelessWidget {
-  const PageLain({Key? key}) : super(key: key);
+// class PageLain extends StatelessWidget {
+//   const PageLain({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(),
+//     );
+//   }
+// }
+
+// class CounterController extends GetxController {
+//   var count = 0;
+
+//   void add() {
+//     count++;
+//     update();
+//   }
+// }
+
+class PageTulisan extends StatelessWidget {
+  final textC = Get.put(TulisanController());
+  PageTulisan({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("Tulisan Page"),
+      ),
+      body: TextField(
+        controller: textC.myC,
+      ),
     );
   }
 }
 
-class CounterController extends GetxController {
-  var count = 0;
-
-  void add() {
-    count++;
-    update();
-  }
+class TulisanController extends GetxController {
+  final myC = TextEditingController();
 }
