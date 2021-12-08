78a3bee095f620e70fe94637a3b7fa419419c0c1
anangfatkur <anangfatkurrr@gmail.com>
2021-12-08 14:10:34 +0700

    penjelasan ever,debounce,interval biasanya untuk security

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -4,4 +4,42 @@ class HomeController extends GetxController {
   var count = 0.obs;
 
   void change() => count++;
+
+  void reset() => count.value = 0;
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    print("oninit");
+    //Note ........Worrkers
+
+    // ever(
+    //   count,
+    //   (_) => print("dijalankan "),
+    // );
+
+    // everAll(
+    //   [count, data],
+    //   (_) => print("dijalankan "),
+    // );
+
+    // once(
+    //   count,
+    //   (_) => print("dijalankan "),
+    // );
+
+    // debounce(
+    //   count,
+    //   (_) => print("dijalankan "),
+    //   time: Duration(seconds: 3),
+    // );
+    
+    interval(
+      count,
+      (_) => print("dijalankan "),
+      time: Duration(seconds: 3),
+    );
+
+    super.onInit();
+  }
 }
